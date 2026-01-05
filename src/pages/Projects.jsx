30807a013654155ede5473333d23a44c7bf72cfc
timmy5575 <timilehinpeter14@gmail.com>
2026-01-05 12:48:46 +0100

    Fix Projects.css import casing

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,7 @@
 import Port from "../component2/heading.jsx"
 import Work from '../component2/work.jsx'
 import Footer from "../components/footer.jsx"
-import "./Projects.css"
+import "./projects.css"
 
 const Projects = () => {
   return (
